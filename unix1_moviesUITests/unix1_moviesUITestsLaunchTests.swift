cb3d7e060897ee444225ce50b09402cb406e7ae8
asuquo60 <53631978+asuquo60@users.noreply.github.com>
2022-02-25 20:24:13 -0600

    Initial Commit

---
+++ unix1_moviesUITests/unix1_moviesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unix1_moviesUITestsLaunchTests.swift
+//  unix1_moviesUITests
+//
+//  Created by Emmanuel  Asuquo  on 2/25/22.
+//
+
+import XCTest
+
+class unix1_moviesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
